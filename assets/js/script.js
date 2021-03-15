62b3b60fd4569be96aa5a3f9c1b12ef4abf4942d
walterego <walter.scimone@gmail.com>
2021-03-15 18:32:38 -0400

    code upgrade complete, opmtimized abilities to prevent event saving on past and present hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
 // waiting for dom ready...
-$(document).ready(function() {
+$(document).ready(function () {
     let present_moment = moment();
 
     // setting current date on header...
     $("#currentDay").text(present_moment.format("LLLL"));
 
-    for(i = 8; i < 19; i++) { 
+    for (i = 8; i < 24; i++) {
         $(".container").append(generateTimeBlock(i, present_moment.hours()));
         $("#text_" + i).val(localStorage.getItem(i));
     }
 });
-
+// generate time block for past, present and future time blocks...
 function generateTimeBlock(index, hour) {
     let block_class = "";
 
-    if(i < hour) {
+    if (i < hour) {
         block_class = "past";
     } else {
-        if(i > hour) {
+        if (i > hour) {
             block_class = "future";
         } else {
             block_class = "present";
@@ -26,15 +26,15 @@ function generateTimeBlock(index, hour) {
 
     let time_block = "<div class='time-block row " + block_class + "'>";
     time_block += "<span class='hour'>" + index + ":00</span>";
-    
+    // disable ability to write events in the past and the present...
     time_block += "<input class='time_block_text' type='text' id='text_" + index + "' name='text_" + i + "'";
-    if(block_class == "past" || block_class == "present") {
+    if (block_class == "past" || block_class == "present") {
         time_block += " disabled ";
     }
     time_block += ">";
-    
+    // disable ability to save events with the button click, in the past and the present...
     time_block += "<button class='saveBtn' onClick='saveItem(" + index + ")'";
-    if(block_class == "past" || block_class == "present") {
+    if (block_class == "past" || block_class == "present") {
         time_block += " disabled ";
     }
     time_block += ">Save</button>";
@@ -42,9 +42,8 @@ function generateTimeBlock(index, hour) {
     
     return time_block;
 }
-
+// save events to local storage... 
 function saveItem(index) {
     let value = $("#text_" + index).val();
-    console.log("going to save " + value + " for index " + index);
     localStorage.setItem(index, value);
 }
